66759c8780420abdf71cf0a58206d2318897d9d7
raheemadamboev <vividricemaryandmax@gmail.com>
2022-05-18 19:57:19 +0500

    add main viewmodel

---
+++ app/src/main/java/xyz/teamgravity/lightsensor/presentation/viewmodel/MainViewModel.kt
@@ -0,0 +1,35 @@
+package xyz.teamgravity.lightsensor.presentation.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import xyz.teamgravity.lightsensor.domain.MeasurableSensor
+import javax.inject.Inject
+
+@HiltViewModel
+class MainViewModel @Inject constructor(
+    private val sensor: MeasurableSensor,
+) : ViewModel() {
+
+    var isDark: Boolean by mutableStateOf(false)
+        private set
+
+    init {
+        initializeSensor()
+    }
+
+    private fun initializeSensor() {
+        sensor.start()
+        sensor.setOnSensorValuesChanged { values ->
+            val lux = values[0]
+            isDark = lux < 60F
+        }
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        sensor.stop()
+    }
+}
